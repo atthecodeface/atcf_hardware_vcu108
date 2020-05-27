0099397ada9c47a288913f58dec9a2f9110bfa0f
Gavin Stark <gjstark@me.com>
2020-05-27 18:41:40 +0100

    [libdesc] Fixed use of instance_types

--- library_desc.py
+++ library_desc.py
@@ -15,10 +15,10 @@ class VCU108Modules(cdl_desc.Modules):
     modules = []
     modules += [ CdlModule("subsys_minimal") ]
     modules += [ CdlModule("vcu108_debug") ]
-    modules += [ CdlModule("vcu108_riscv",   blah={"riscv_i32_minimal_generic":"riscv_i32_minimal"}, cdl_filename="vcu108_riscv_generic") ]
-    modules += [ CdlModule("vcu108_riscv_3", blah={"riscv_i32_minimal_generic":"riscv_i32_minimal3"}, cdl_filename="vcu108_riscv_generic") ]
-    modules += [ CdlModule("tb_vcu108_debug",   blah={"vcu108_generic":"vcu108_debug"},   cdl_filename="tb_vcu108_generic", src_dir=tb_src_dir) ]
-    modules += [ CdlModule("tb_vcu108_riscv",   blah={"vcu108_generic":"vcu108_riscv"},   cdl_filename="tb_vcu108_generic", src_dir=tb_src_dir) ]
-    modules += [ CdlModule("tb_vcu108_riscv_3", blah={"vcu108_generic":"vcu108_riscv_3"}, cdl_filename="tb_vcu108_generic", src_dir=tb_src_dir) ]
+    modules += [ CdlModule("vcu108_riscv",   instance_types={"riscv_i32_minimal_generic":"riscv_i32_minimal"}, cdl_filename="vcu108_riscv_generic") ]
+    modules += [ CdlModule("vcu108_riscv_3", instance_types={"riscv_i32_minimal_generic":"riscv_i32_minimal3"}, cdl_filename="vcu108_riscv_generic") ]
+    modules += [ CdlModule("tb_vcu108_debug",   instance_types={"vcu108_generic":"vcu108_debug"},   cdl_filename="tb_vcu108_generic", src_dir=tb_src_dir) ]
+    modules += [ CdlModule("tb_vcu108_riscv",   instance_types={"vcu108_generic":"vcu108_riscv"},   cdl_filename="tb_vcu108_generic", src_dir=tb_src_dir) ]
+    modules += [ CdlModule("tb_vcu108_riscv_3", instance_types={"vcu108_generic":"vcu108_riscv_3"}, cdl_filename="tb_vcu108_generic", src_dir=tb_src_dir) ]
     pass
 
